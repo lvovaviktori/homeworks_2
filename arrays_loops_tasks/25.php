4fa24e05d6f64e7d1eebb7865895ed9b782f2886
viktoriya <viktoriya>
2017-02-03 12:25:41 +0200

    change

--- arrays_loops_tasks/25.php
+++ arrays_loops_tasks/25.php
@@ -1,11 +1,20 @@
 <?php
-
+/**
+ * 25. Ваше задание создать массив, наполнить это случайными значениями (функция rand),
+ * найти максимальное и минимальное значение и поменять их местами.
+ */
 for($i=0; $i<10; $i++) {
     $arr[$i] = rand(0,10);
 }
 print_r($arr);
+
 $max_arr=max($arr);
 $min_arr=min($arr);
 
-echo $max_arr."\n";
-echo $min_arr;
+$min_key = array_search($min_arr, $arr);
+$max_key = array_search($max_arr, $arr);
+
+$arr[$min_key] = $max_arr;
+$arr[$max_key] = $min_arr;
+
+print_r($arr);
